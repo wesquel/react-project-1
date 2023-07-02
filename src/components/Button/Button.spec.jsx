af77c938c89bb16eb0ab9a93519cdce07190651e
Weslley Addson <wesquel@hotmail.com>
2023-07-02 12:10:30 -0300

    eslint config

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -5,7 +5,8 @@ import { Button } from ".";
 
 describe("<Button/>", () => {
   it("should render the button with the text 'LoadMore'", () => {
-    render(<Button text="load more" />);
+    const fn = jest.fn();
+    render(<Button text="load more" disabled={false} onClick={fn} />);
     expect.assertions(1);
     const button = screen.getByRole("button", { name: /load more/i });
     expect(button).toHaveAttribute("class", "button");
